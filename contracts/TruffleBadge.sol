471566bb4ae0a38d5c00573b719d0641ad304c09
Ben Meredith <whizkid120@gmail.com>
2020-11-18 00:06:23 -0600

    Added contract functionality + fixed config to have the correct compiler version

--- contracts/TruffleBadge.sol
+++ contracts/TruffleBadge.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity ^0.6.2;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract TruffleBadge {
-  constructor() ERC721("TruffleBadge", "TRFL") public {
-    
-  }
+contract TruffleBadge is ERC721 {
+    constructor() ERC721("TruffleBadge", "TRFL") public {
+    }
+
+    function awardBadge(address attendee) public returns (uint256) {
+        uint badgeId = 888;
+        _mint(attendee, badgeId);
+        return badgeId;
+    }
 }
