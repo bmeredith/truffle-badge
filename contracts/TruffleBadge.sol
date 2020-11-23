a803faeda45a048f6f5830ae35f0370f86fcd357
Ben Meredith <whizkid120@gmail.com>
2020-11-22 19:21:12 -0600

    Added ability to remove a badge. Limited only 1 badge per attendee.

--- contracts/TruffleBadge.sol
+++ contracts/TruffleBadge.sol
@@ -6,18 +6,48 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract TruffleBadge is ERC721 {
     using Counters for Counters.Counter;
-    Counters.Counter private _badgeIds;
+    Counters.Counter private _badgeIdCount;
+    
+    mapping (address => uint256) public badgeIds;
 
     constructor() ERC721("TruffleBadge", "TRFL") public {
     }
 
     function awardBadge(address attendee, string memory attendenceBadgeURI) public returns (uint256) {
-        _badgeIds.increment();
+        bool hasBadge = _hasBadge(attendee);
+        if(hasBadge)
+        {
+            revert("Only one badge can be awarded per account.");
+        }
 
-        uint256 newBadgeId = _badgeIds.current();
+        uint256 newBadgeId = _badgeIdCount.current();
         _mint(attendee, newBadgeId);
         _setTokenURI(newBadgeId, attendenceBadgeURI);
+        _badgeIdCount.increment();
 
         return newBadgeId;
+    }
+
+    function removeBadge(address attendee) public {
+        bool hasBadge = _hasBadge(attendee);
+        if(!hasBadge)
+        {
+            return;
+        }
+
+        uint256 badge = _getBadge(attendee);
+        _burn(badge);
+        delete badgeIds[attendee];
+        _badgeIdCount.decrement();
+
+        return;
+    }
+
+    function _getBadge(address attendee) private returns (uint256) {
+        return badgeIds[attendee];
+    }
+
+    function _hasBadge(address attendee) private returns (bool) {
+        return balanceOf(attendee) > 0;
     }
 }
--- test/truffle_badge.js
+++ test/truffle_badge.js
@@ -1,4 +1,5 @@
 const TruffleBadge = artifacts.require("TruffleBadge");
+const { reverts } = require("truffle-assertions");
 
 contract("TruffleBadge", function(accounts) {
     
@@ -8,7 +9,7 @@ contract("TruffleBadge", function(accounts) {
   });
 
   describe("Creation", () => {
-    it("award badge", async () => {
+    it("should award badge", async () => {
       const meta = "META";
       await badge.awardBadge(accounts[1], meta);
       assert.equal(
@@ -17,6 +18,28 @@ contract("TruffleBadge", function(accounts) {
       );
 
       await badge.awardBadge(accounts[2], meta);
+    });
+
+    it("should only allow 1 badge to be awarded", async () => {
+      const meta = "META";
+      const account = accounts[1];
+
+      await badge.awardBadge(account, meta);
+
+      await reverts(
+        badge.awardBadge(account, meta),
+        "Only one badge can be awarded per account."
+      );
+    });
+  });
+
+  describe("Removal", () => {
+    it("should remove badge", async () => {
+      const meta = "META";
+      const account = accounts[1];
+      await badge.awardBadge(account, meta);
+      await badge.removeBadge(account);
+      assert.equal(await badge.balanceOf(account), 0);
     });
   });
 });
